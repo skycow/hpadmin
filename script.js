83bdd250fc7783133b2e03eaa125b1f847bfd123
Skyler Cowley <skyler.cowley@gmail.com>
2017-04-13 09:34:09 -0600

    start stop button

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ $(function(){
 	  event.preventDefault()
 	  $(".settings").hide();
 	  $(".results").show();
-	});	
+	});
+	$("button").click(function(event){
+		if($("button").html() == "Start"){
+			$("button").addClass("btn-danger").removeClass("btn-success")
+			$("button").html("Stop")
+		}else if($("button").html() == "Stop") {
+			$("button").addClass("btn-success").removeClass("btn-danger")
+			$("button").html("Start")			
+		}
+	});
 }
 )
-
